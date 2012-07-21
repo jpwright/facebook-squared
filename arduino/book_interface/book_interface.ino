145ee1e0b65950bc1b94ca835977ff483f59e33b
Jeremy Blum <jeremy@jeremyblum.com>
2012-07-21 16:24:07 -0400

    Single LCD tested and Working

--- arduino/book_interface/book_interface.ino
+++ arduino/book_interface/book_interface.ino
@@ -1,8 +1,26 @@
 //Jeremy Blum & Jason Wright
 //Facebook NYC Summer Hack-a-Thon 7/21/2012
 
+//Definitions
 #define DEBUG Serial
 #define XBEE Serial1
+
+/* 
+  LCD
+  The circuit:
+ * LCD RS pin to digital pin 27
+ * LCD EN pin to digital pin 26
+ * LCD D4 pin to digital pin 25
+ * LCD D5 pin to digital pin 24
+ * LCD D6 pin to digital pin 23
+ * LCD D7 pin to digital pin 22
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ */
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(27, 26, 25, 24, 23, 22);
 
 void setup()
 {
@@ -16,6 +34,11 @@ void setup()
   XBEE.begin(9600);
   
   //Setup interface to the multiple Parallel LCD interfaces
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Jeremy's Display");
+  lcd.setCursor(0,1);
+  lcd.print("The PhysiBook");
 }
 void loop()
 {
