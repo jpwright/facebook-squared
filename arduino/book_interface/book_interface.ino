d5ecb18c134d548adfd48b4869d9a153f9e3785e
Jeremy Blum <jeremy@jeremyblum.com>
2012-07-21 15:03:48 -0400

    Arduino Debug and XBee working

--- arduino/book_interface/book_interface.ino
+++ arduino/book_interface/book_interface.ino
@@ -1,15 +1,25 @@
 //Jeremy Blum & Jason Wright
 //Facebook NYC Summer Hack-a-Thon 7/21/2012
 
-setup()
+#define DEBUG Serial
+#define XBEE Serial1
+
+void setup()
 {
   //Setup interface to the Adafruit Visual Display
   //http://www.adafruit.com/products/338
   
+  //Serial interface to Computer Via USB (for debugging only)
+  DEBUG.begin(9600);
+  
+  //Serial interface to XBEE for Data transmit and receive
+  XBEE.begin(9600);
+  
   //Setup interface to the multiple Parallel LCD interfaces
 }
-void()
+void loop()
 {
-  //Serial Communication Handling
-  
+  //Serial Communication Debug
+  //DEBUG.println("Test to the Debug");
+  //XBEE.println("Test via XBEE");
 }
